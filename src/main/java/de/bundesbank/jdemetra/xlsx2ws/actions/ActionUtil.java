8f1c3e5d444b5d4c23a588992e2bb839a6ca21b9
Thomas Witthohn <thomas.witthohn@bundesbank.de>
2021-08-31 14:24:39 +0200

    Removed JavaFX components

--- src/main/java/de/bundesbank/jdemetra/xlsx2ws/actions/ActionUtil.java
+++ src/main/java/de/bundesbank/jdemetra/xlsx2ws/actions/ActionUtil.java
@@ -6,6 +6,7 @@
 package de.bundesbank.jdemetra.xlsx2ws.actions;
 
 import de.bundesbank.jdemetra.xlsx2ws.dto.ISetting;
+import java.awt.Component;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
@@ -13,10 +14,8 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.prefs.Preferences;
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
-import javafx.stage.FileChooser;
-import javafx.stage.Window;
+import javax.swing.JFileChooser;
+import org.jfree.ui.ExtensionFileFilter;
 import org.netbeans.api.progress.ProgressHandle;
 import org.openide.util.NbPreferences;
 
@@ -28,11 +27,10 @@ public class ActionUtil {
 
     public static final String LAST_FOLDER = "lastFolder";
 
-    private static final FileChooser FILE_CHOOSER = new FileChooser();
+    private static final JFileChooser FILE_CHOOSER = new JFileChooser();
 
     static {
-        FILE_CHOOSER.getExtensionFilters().add(new FileChooser.ExtensionFilter("Spreadsheet file", "*.xlsx"));
-        new JFXPanel();
+        FILE_CHOOSER.addChoosableFileFilter(new ExtensionFileFilter("Spreadsheet file", "*.xlsx"));
     }
 
     public static final void save(String progressMessage, HashMap<String, ISetting> hashMap, BiConsumer<File, HashMap<String, ISetting>> consumer) {
@@ -53,7 +51,7 @@ public class ActionUtil {
         });
     }
 
-    private static File chooseFile(Function<Window, File> function) {
+    private static File chooseFile(Function<Component, Integer> function) {
 
         Preferences preferences = NbPreferences.forModule(ActionUtil.class);
         File startingDirectory = new File(preferences.get(ActionUtil.LAST_FOLDER, System.getProperty("user.home")));
@@ -63,26 +61,27 @@ public class ActionUtil {
             startingDirectory = new File(System.getProperty("user.home"));
         }
         if (startingDirectory.exists()) {
-            FILE_CHOOSER.setInitialDirectory(startingDirectory);
+            FILE_CHOOSER.setCurrentDirectory(startingDirectory);
         }
-        File file = function.apply(null);
-        if (file != null) {
-            preferences.put(ActionUtil.LAST_FOLDER, file.getParent());
+        Integer response = function.apply(null);
+        File file = null;
+        if (response == JFileChooser.APPROVE_OPTION) {
+            file = FILE_CHOOSER.getSelectedFile();
+            if (file != null) {
+                preferences.put(ActionUtil.LAST_FOLDER, file.getParent());
+            }
         }
         return file;
     }
 
     private static void runFunctionWithProgressBar(String progressMessage, Runnable c) {
-
-        Platform.runLater(() -> {
-            ProgressHandle progressHandle = ProgressHandle.createHandle(progressMessage);
-            try {
-                progressHandle.start();
-                c.run();
-            } finally {
-                progressHandle.finish();
-            }
-        });
+        ProgressHandle progressHandle = ProgressHandle.createHandle(progressMessage);
+        try {
+            progressHandle.start();
+            c.run();
+        } finally {
+            progressHandle.finish();
+        }
     }
 
 }
